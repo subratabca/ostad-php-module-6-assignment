f5f7519387f9b2ec5a9730b4ed08ce656369c56b
subratabca22 <subratabca22@gmail.com>
2023-11-03 20:41:08 +0600

    initial commit

---
+++ task-2.sql
@@ -0,0 +1,4 @@
+SELECT OI.order_id, P.name AS product_name, OI.quantity,(OI.quantity * OI.unit_price) AS total_amount
+FROM Order_Items OI
+INNER JOIN Products P ON OI.product_id = P.product_id
+ORDER BY OI.order_id ASC;
